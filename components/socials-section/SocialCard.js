bc2b1f73fcf7ac0932aba7bdb531f8eb99cd7c47
stian <stianbekkelund@hotmail.com>
2021-06-03 22:45:57 +0200

    changed color on social card

--- components/socials-section/SocialCard.js
+++ components/socials-section/SocialCard.js
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 const SocialCard = ({ title, icon, link }) => {
   return (
-    <div className="flex flex-col justify-evenly items-center bg-gray-3 rounded-md text-gray-1 text-base shadow-md my-3 mx-1 w-1/2 h-40">
+    <div className="flex flex-col justify-evenly items-center bg-gray-4 rounded-md text-gray-1 text-base shadow-md my-3 mx-1 w-1/2 h-40">
       <h4>{title}</h4>
       <div>{icon}</div>
       <div className="self-end">
         <Link href={link}>
-          <a className="block self-start font-body text-center w-20 mx-2 border rounded-full py-1 hover:bg-gray-1 hover:text-gray-3">
+          <a className="block self-start font-body text-center w-20 mx-2 border rounded-full py-1 hover:bg-gray-1 hover:text-gray-4">
             Visit
           </a>
         </Link>
