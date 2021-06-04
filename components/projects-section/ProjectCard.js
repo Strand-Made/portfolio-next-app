d1b91009ef994e6e54d2dec979d876a973bc2183
stian <stianbekkelund@hotmail.com>
2021-06-04 11:27:00 +0200

    project card styled. need animations & bg

--- components/projects-section/ProjectCard.js
+++ components/projects-section/ProjectCard.js
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 const ProjectCard = () => {
-  return <div></div>;
+  const [mouseHover, setMouseHover] = useState(false);
+
+  return (
+    <div
+      onMouseEnter={() => setMouseHover(true)}
+      onMouseLeave={() => setMouseHover(false)}
+      className="block rounded-2xl shadow-md bg-gray-1 h-48 w-2/3"
+    >
+      Card
+      {mouseHover ? (
+        <div className="bg-indigo-3 h-full">
+          <h4>Project</h4>
+          <Link href="/">
+            <a className="text-gray-300">View</a>
+          </Link>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default ProjectCard;
--- components/projects-section/ProjectsSection.js
+++ components/projects-section/ProjectsSection.js
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import ProjectsContainer from "./ProjectsContainer";
+import ProjectCard from "./ProjectCard";
 import { IoMdInformationCircle } from "react-icons/io";
 
 const ProjectsSection = () => {
   return (
     <ProjectsContainer>
       <h2 className="text-3xl text-center text-gray-1">Projects</h2>
-      <div>CARDS</div>
+      <div className="flex flex-col items-center my-3">
+        <ProjectCard />
+      </div>
       <div className="flex items-center">
         <IoMdInformationCircle className="text-5xl" />
-        <p className="font-body text-base max-w-prose">
+        <p className="font-body text-xs max-w-prose">
           You are free to contact me to see more projects, or you can check out
           my github&nbsp;
           <Link href="https://github.com/Strand-Made/">
