6a878eb779ed1dc7c25ce2b88484b13c2a2fbc93
stian <stianbekkelund@hotmail.com>
2021-06-05 17:39:24 +0200

    added grey to config & added custom outline to inputs

--- components/contact-page/FormInputs.js
+++ components/contact-page/FormInputs.js
@@ -5,7 +5,8 @@ const FormInputs = ({ id, label }) => {
         {label}
       </label>
       <input
-        className="font-body text-base text-gray-3 w-full rounded-lg max-w-sm py-1 border border-gray-4"
+        className="font-body text-base text-gray-3 w-full rounded-lg max-w-sm py-1 border border-gray-5 
+        hover:border-indigo-2 focus:border-indigo-3 hover:border-2 focus:ring focus:ring-indigo-2 outline-none"
         id={id}
         type="text"
       />
--- components/contact-page/FormTextArea.js
+++ components/contact-page/FormTextArea.js
@@ -7,7 +7,8 @@ const FormTextArea = ({ label, id }) => {
       <textarea
         rows="5"
         cols="33"
-        className="font-body p-2 text-sm rounded-lg max-w-sm py-1 border border-gray-4"
+        className="font-body p-2 text-sm rounded-lg max-w-sm py-1 border border-gray-5 resize-none outline-none border border-gray-5 
+        hover:border-indigo-2 focus:border-indigo-3 hover:border-2 focus:ring focus:ring-indigo-2 outline-none"
         placeholder="A sweet message"
         id={id}
       ></textarea>
