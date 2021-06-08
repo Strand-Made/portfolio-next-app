f6750733ac33843ea52ec69207557ab9ccbebee5
stian <stianbekkelund@hotmail.com>
2021-06-08 15:56:13 +0200

    api for project data
    Created api for projects data

---
+++ pages/api/projects/[id].js
@@ -0,0 +1,10 @@
+import { projects } from "../../../public/projectsData";
+
+export default function handler({ query: { id } }, res) {
+  const filtered = projects.filter((project) => project.id === id);
+  if (filtered.length > 0) {
+    res.status(200).json(filtered[0]);
+  } else {
+    res.status(404).json({ message: `Project with id ${id} not found` });
+  }
+}
