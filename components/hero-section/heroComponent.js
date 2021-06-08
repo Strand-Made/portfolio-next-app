1ac5dc452f627a22826418a8877c531c64a27e1c
stian <stianbekkelund@hotmail.com>
2021-06-08 15:53:25 +0200

    Font sans
    Font sans

--- components/hero-section/heroComponent.js
+++ components/hero-section/heroComponent.js
@@ -2,7 +2,7 @@ import ProfileImage from "./ProfileImage";
 const HeroComponent = () => {
   return (
     <section className="w-full flex flex-col items-center ">
-      <h1 className="text-4xl max-w-sm text-indigo-3 font-bold text-center ">
+      <h1 className="text-4xl font-sans max-w-sm text-indigo-3 font-bold text-center ">
         Frontend Developer & chill guy
       </h1>
       <ProfileImage />
