1614d8b5bcea60b2671c3fc035bdae1597bdaf47
stian <stianbekkelund@hotmail.com>
2021-06-08 15:52:56 +0200

    Changed text styles for about card
    Changed h4 tag to h3 & font bold

--- components/about-section/AboutCardContent.js
+++ components/about-section/AboutCardContent.js
@@ -1,14 +1,14 @@
 const AboutCardContent = () => {
   return (
     <div>
-      <h3 className="text-center text-gray-2 text-lg">Languages</h3>
+      <h3 className="text-center font-bold text-gray-2 text-lg">Languages</h3>
       <ul className="text-base flex font-body mb-3">
         <li>Javascript,</li>
         <li> CSS,</li>
         <li>HTML &</li>
         <li>Sass</li>
       </ul>
-      <h4 className="text-center text-gray-2 text-lg">Dev Tools</h4>
+      <h3 className="text-center font-bold text-gray-2 text-lg">Dev Tools</h3>
       <ul className="font-body">
         <li>ReactJS</li>
         <li>NextJS</li>
