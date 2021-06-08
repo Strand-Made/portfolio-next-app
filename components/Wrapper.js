c88e4ab8b7a71d31ab7288fddf1a23d6cebc45c2
stian <stianbekkelund@hotmail.com>
2021-06-08 15:54:40 +0200

    Mobile responsive nav
    Added wrapper for navbar to properly have overflow hidden

---
+++ components/Wrapper.js
@@ -0,0 +1,5 @@
+const Wrapper = ({ children }) => {
+  return <div className="relative w-100 overflow-hidden">{children}</div>;
+};
+
+export default Wrapper;
--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import useOnClickOutside from "../../hooks";
 import Link from "next/link";
 import { HiMenu, HiX } from "react-icons/hi";
 import Logo from "../logo/Logo";
@@ -7,21 +8,44 @@ import Button from "../Button";
 const Navbar = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
+  const styles = {
+    mobileMenuStyle:
+      "flex flex-col space-y-5 transition-all  ease-in-out duration-700 bg-indigo-1 text-center absolute h-screen left-0 right-0 top-0 py-10 z-40 transform ",
+    mobileMenuContainer: "flex relative w-full transition-all sm:hidden",
+  };
+
   return (
-    <nav className="mx-2 font-sans text-indigo-3 ">
+    <nav className="font-sans text-indigo-3 w-full">
       <div className="lg:container lg:mx-auto">
         <div className="flex flex-row justify-between w-full">
           <div>
             <Logo theme="regular" href="/" />
           </div>
-          <div className="flex items-center sm:hidden">
-            <button className="" onClick={handleClick}>
+          <div className={`${styles.mobileMenuContainer}`}>
+            <button className="absolute right-0 z-50 " onClick={handleClick}>
               {click ? (
-                <HiX className="text-4xl" />
+                <HiX className="text-4xl fill-current text-gray-1" />
               ) : (
                 <HiMenu className="text-4xl" />
               )}
             </button>
+
+            <ul
+              className={`${styles.mobileMenuStyle} ${
+                click ? "translate-x-0" : "translate-x-full "
+              } "`}
+            >
+              <li className="text-gray-1">
+                <Link href="/">
+                  <a>Projects</a>
+                </Link>
+              </li>
+              <li className="text-gray-1">
+                <Link href="/contact">
+                  <a>Contact me</a>
+                </Link>
+              </li>
+            </ul>
           </div>
           <ul className="hidden sm:flex items-center space-x-3">
             <li>Projects</li>
@@ -31,20 +55,6 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-      {click ? (
-        <ul className="bg-indigo-3 text-center">
-          <li className="text-gray-1">
-            <Link href="/">
-              <a>Projects</a>
-            </Link>
-          </li>
-          <li className="text-gray-1">
-            <Link href="/contact">
-              <a>Contact me</a>
-            </Link>
-          </li>
-        </ul>
-      ) : null}
     </nav>
   );
 };
