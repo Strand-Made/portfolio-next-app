e6383aea7b576692feea6f8be26dda2080651eb6
stian <stianbekkelund@hotmail.com>
2021-06-10 21:33:37 +0200

    made project card show on tap

--- components/projects-section/ProjectCard.js
+++ components/projects-section/ProjectCard.js
@@ -1,20 +1,25 @@
 import Link from "next/link";
+
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 const ProjectCard = ({ project }) => {
-  const cardAnimationVariants = {
-    initial: {
-      opacity: 0,
-    },
-    hover: {
-      opacity: 1,
-    },
+  const transition = { duration: 1, ease: "easeInOut" };
+
+  const cardDisplayVariants = {
+    initial: { opacity: 0, y: 100 },
+    enter: { opacity: 1, y: 0, transition },
+    leave: { opacity: 0, y: -100, transition },
   };
 
   return (
-    <div
+    <motion.li
+      initial="leave"
+      animate="enter"
+      exit="leave"
+      variants={cardDisplayVariants}
       key={project.id}
-      className="block relative rounded-2xl transition-shadow duration-700 ease-in-out shadow-lg hover:shadow-sm relative h-56 md:h-72 max-h-96 max-w-lg w-2/3 max-w-xs"
+      className="block relative rounded-2xl transition-shadow duration-700 ease-in-out 
+      shadow-lg hover:shadow-sm relative h-56 md:h-72 max-h-96 max-w-lg w-2/3 max-w-xs"
     >
       <Image
         className="absolute top-0 right-0 left-0 rounded-2xl"
@@ -22,24 +27,41 @@ const ProjectCard = ({ project }) => {
         desc={project.description}
         layout="fill"
       />
+      <CardContent project={project} />
+    </motion.li>
+  );
+};
 
-      <motion.div
-        initial="initial"
-        whileHover="hover"
-        variants={cardAnimationVariants}
-        transition={{ duration: 0.4 }}
-        className="bg-indigo-3 bg-opacity-50 flex flex-col items-center justify-between py-5 absolute z-index-50 w-full top-0 rounded-2xl h-full"
-      >
-        <h4 className="text-xl">{project.title}</h4>
-        <div>
-          <Link href="/project[id]" as={`/project/${project.id}`}>
-            <a className="block bg-pink-cta text-pink-white font-body text-base text-center w-36 shadow-md py-1 rounded-full hover:bg-indigo-3 focus:bg-indigo-3 focus:shadow hover:shadow">
-              View
-            </a>
-          </Link>
-        </div>
-      </motion.div>
-    </div>
+const CardContent = ({ project }) => {
+  const transition = { duration: 0.4, ease: "easeOut" };
+
+  const cardAnimationVariants = {
+    initial: { opacity: 0, transition },
+    hover: { opacity: 1, transition },
+  };
+  const [current, cycle] = useCycle("initial", "hover");
+  return (
+    <motion.div
+      initial="initial"
+      whileHover="hover"
+      animate={current}
+      onTap={() => cycle()}
+      variants={cardAnimationVariants}
+      className="bg-indigo-3  flex flex-col items-center justify-between py-5
+         absolute z-index-10 w-full top-0 rounded-2xl h-full"
+    >
+      <h4 className="text-xl">{project.title}</h4>
+      <div>
+        <Link href="/project[id]" as={`/project/${project.id}`}>
+          <a
+            className="block bg-indigo-1 text-gray-1 text-center
+             w-36 shadow-md py-1 rounded-full hover:bg-indigo-2 focus:bg-indigo-2 focus:shadow hover:shadow"
+          >
+            View
+          </a>
+        </Link>
+      </div>
+    </motion.div>
   );
 };
 
