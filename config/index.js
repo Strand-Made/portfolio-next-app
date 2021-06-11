029e18b6272a104db75c7bf3c58697b912a51c90
stian <stianbekkelund@hotmail.com>
2021-06-11 13:55:58 +0200

    fixed wrong commit

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://bango-portfolio-9fyvtz0ji-strand-made.vercel.app";
+  : "https://bango-portfolio-vercel.app";
