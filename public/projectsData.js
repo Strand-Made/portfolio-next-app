f05c3bafcb41785127c7e87dd6fdc2f3976273a4
stian <stianbekkelund@hotmail.com>
2021-06-11 13:32:04 +0200

    added data & images

--- public/projectsData.js
+++ public/projectsData.js
@@ -1,23 +1,31 @@
 export const projects = [
   {
-    title: "E-commerce",
+    title: "HomeSmart",
     id: "1",
-    image: "/exampleWebsite.jpg",
+    url: null,
+    image: "/homesmart.jpg",
+    shortDesc: "E-commerce site using Strapi api, made with JS",
     description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo",
+      "E-commerce website made with HTML, TailwindCSS & JS using data from a strapi headless-cms. Users are able to add and remove products from favourites using local storage. Admin user can add, edit and delete products using the admin dashboard. The strapi cms is currently not live, therefore no link to the website, but please take a look at the repository ",
   },
   {
     title: "SpaceX Launch center",
     id: "2",
-    image: "/exampleWebsite.jpg",
+    url: "http://www.bango.one/project-exam/",
+    image: "/spacex-center.jpg",
+    shortDesc:
+      "Website using SpaceX api to display launches, missions & Rockets",
     description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo",
+      "This website uses the SpaceX api to get data and displays upcoming launches, information on missions and details about spaceX rockets. This website is made with html, css and js.",
   },
   {
-    title: "Github search app",
+    title: "Github Search app",
     id: "3",
-    image: "/exampleWebsite.jpg",
+    url: "https://bango-github-search.netlify.app/",
+    image: "/bango-github.jpg",
+    shortDesc:
+      "React App where you can search for Github user and get repo statistics.",
     description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo",
+      "React app where you can search for Github users and get their repo statistics. Made with styled-components and fusion-charts for data display. This app also uses Auth0 for secure login with twitter and github. ",
   },
 ];
