2b042cca6cedbb00c4764c7a14019b77cf17f21e
stian <stianbekkelund@hotmail.com>
2021-06-13 22:40:03 +0200

    fixed blob width

--- components/about-section/AboutContent.js
+++ components/about-section/AboutContent.js
@@ -5,8 +5,8 @@ const AboutContent = () => {
     <>
       <h2 className="text-3xl font-sans text-center mb-2 ">👋Hi! I am Stian</h2>
       <div className="relative flex justify-center">
-        <BlobCenter className="hidden md:inline-block mix-blend-soft-light opacity-10 absolute z-0 transform -translate-y-40 translate-x-52 lg:translate-x-full " />
-        <BlobCenter className="hidden md:inline-block mix-blend-soft-light opacity-10 absolute z-0 transform rotate-180 -translate-y-60 -translate-x-72 lg:-translate-x-full " />
+        <BlobCenter className="hidden md:inline-block mix-blend-soft-light opacity-10 absolute z-0 transform -translate-y-40  translate-x-full " />
+        <BlobCenter className="hidden md:inline-block mix-blend-soft-light opacity-10 absolute z-0 transform rotate-180 -translate-y-60  -translate-x-full " />
 
         <p className="max-w-prose lg:text-lg">
           I am a frontend developer who loves good user experiences. I have a
