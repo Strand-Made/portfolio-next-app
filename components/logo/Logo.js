fd12c6a0f7f54712a83d58554d8f7d22efc11e23
stian <stianbekkelund@hotmail.com>
2021-07-11 19:12:28 +0200

    refactored logo component

--- components/logo/Logo.js
+++ components/logo/Logo.js
@@ -2,25 +2,14 @@ import Link from "next/link";
 
 const Logo = ({ href, theme }) => {
   const logoText = "Strand";
-  if (theme === "regular") {
-    return (
-      <Link href={href}>
-        <a className="text-3xl font-display text-indigo-3 font-bolder">
-          {logoText}
-        </a>
-      </Link>
-    );
-  } else if (theme === "white") {
-    return (
-      <Link href={href}>
-        <a className="text-3xl font-display text-gray-1 font-bolder">
-          {logoText}
-        </a>
-      </Link>
-    );
-  } else {
-    throw "Please use regular or white in theme ";
-  }
+  const styles = `text-3xl font-display font-bolder ${
+    theme === "regular" ? "text-indigo-3" : "text-gray-1"
+  }`;
+  return (
+    <Link href={href}>
+      <a className={styles}>{logoText}</a>
+    </Link>
+  );
 };
 
 export default Logo;
