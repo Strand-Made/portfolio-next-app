9d5f6e3d3a6ac37dba2cac016a3a20615875fb32
Stian <60408983+Strand-Made@users.noreply.github.com>
2021-07-15 11:26:03 +0200

    redesigned contact link
    redesigned contact link & added linkedin link

--- components/Footer.js
+++ components/Footer.js
@@ -1,37 +1,35 @@
 import Link from "next/link";
 import Container from "./Container";
 import Logo from "./logo/Logo";
-import { IoLogoGithub, IoLogoTwitter } from "react-icons/io";
+import { IoLogoGithub, IoLogoTwitter, IoLogoLinkedin } from "react-icons/io";
 const Footer = () => {
   return (
     <footer className="bg-gray-3 mt-6 py-5">
       <Container>
         <div className="flex flex-col h-32 justify-between">
-          <ul className="flex w-full justify-center space-x-10 text-gray-1 font-body">
-            <li>
-              <Link href="https://www.github.com/Strand-Made">
-                <a>Github</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact">
-                <a>Contact</a>
-              </Link>
-            </li>
-          </ul>
+          <div className="flex flex-col items-center md:items-end">
+            <Link href="/contact">
+              <a className="flex flex-col justify-evenly items-center bg-gray-4 hover:bg-gray-5 rounded-md text-gray-1 text-base shadow-md my-3 mx-1 w-36 h-16">
+                Contact me
+              </a>
+            </Link>
+          </div>
           <div className="flex justify-between items-end">
             <Logo theme="white" href="/" />
-            <div className="flex space-x-5 text-gray-1">
+            <div className="flex space-x-1 md:space-x-5 text-gray-1">
               <Link href="https://www.twitter.com/stianstrand3">
                 <a className="flex flex-col items-center">
                   <IoLogoTwitter className="text-4xl" />
-                  Twitter
                 </a>
               </Link>
               <Link href="https://www.github.com/StrandMade">
                 <a className="flex flex-col items-center">
                   <IoLogoGithub className="text-4xl" />
-                  Github
+                </a>
+              </Link>
+              <Link href="https://www.linkedin.com/in/stian-strand-877b9913b/">
+                <a className="flex flex-col items-center">
+                  <IoLogoLinkedin className="text-4xl" />
                 </a>
               </Link>
             </div>
