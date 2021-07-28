5d59690c8c65be12a1b6697389bc1fca84b737e0
Stian <60408983+Strand-Made@users.noreply.github.com>
2021-07-28 14:29:14 +0200

    refactored card component
    moved card content to its own component

--- components/projects-section/ProjectCard.js
+++ components/projects-section/ProjectCard.js
@@ -1,8 +1,8 @@
-import Link from "next/link";
 import Image from "next/image";
 import { useEffect } from "react";
-import { motion, useCycle, useAnimation } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import CardContent from "./CardContent";
 
 const ProjectCard = ({ project }) => {
   const { description, image } = project.fields;
@@ -43,41 +43,6 @@ const ProjectCard = ({ project }) => {
       />
       <CardContent project={project} />
     </motion.li>
-  );
-};
-
-const CardContent = ({ project }) => {
-  const transition = { duration: 0.4, ease: "easeOut" };
-  const { title, shortDesc } = project.fields;
-
-  const cardAnimationVariants = {
-    initial: { opacity: 0, transition },
-    hover: { opacity: 1, transition },
-  };
-  const [current, cycle] = useCycle("initial", "hover");
-  return (
-    <motion.div
-      initial="initial"
-      whileHover="hover"
-      animate={current}
-      onTap={() => cycle()}
-      variants={cardAnimationVariants}
-      className="bg-gradient-to-b from-indigo-1 to-indigo-3 flex flex-col items-center justify-between py-5
-         absolute z-index-10 w-full top-0 rounded-2xl h-full"
-    >
-      <h4 className="text-xl font-bold">{title}</h4>
-      <p className="text-lg max-w-prose px-3">{shortDesc}</p>
-      <div>
-        <Link href="/project[id]" as={`/project/${project.sys.id}`}>
-          <a
-            className="block bg-indigo-1 text-gray-1 text-center
-             w-36 shadow-md py-1 transition-colors duration-300 ease-linear rounded-full hover:bg-indigo-2 focus:bg-indigo-2 focus:shadow hover:shadow"
-          >
-            View
-          </a>
-        </Link>
-      </div>
-    </motion.div>
   );
 };
 
